dd454447c6ac7d5353e08b51858fe6729c0f004e
Brenda <ziruhe@gmail.com>
2023-09-28 03:31:04 +0000

    Sep 27, 2023, 8:31 PM

---
+++ Assignment02-module3/js/01-larger-or-smaller.js
@@ -0,0 +1,11 @@
+document.write(`<h3>Larger or Smaller App</h3>`)
+let firstNumber = parseInt(prompt('Please enter the first integer: '));
+let secondNumber = parseInt(prompt('Please enter the second integer: '));
+if(firstNumber > secondNumber) {
+    document.write(`The larger number is the first number: ${firstNumber}`)
+} else if(secondNumber > firstNumber) {
+    document.write(`The larger number is the second number: ${secondNumber}`)
+} else {
+    document.write(`The two numbers are equal!`)
+
+}
