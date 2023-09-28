2290834a0d359289d44bbce28ead44efd1b28f64
Brenda <ziruhe@gmail.com>
2023-09-28 03:53:02 +0000

    Sep 27, 2023, 8:53 PM

--- Assignment02-module3/js/02-word-translator.js
+++ Assignment02-module3/js/02-word-translator.js
@@ -1,1 +1,11 @@
 document.write(`<h2>The World Translator App</h2>`)
+languageCode = prompt("Please enter the language code: ")
+if(languageCode === 'es') {
+    console.log("Hello World translated in Spanish is: Hola Mundo")
+} else if(languageCode === 'de') {
+    console.log("Hello World translated in German is: Hallo Welt")
+} else if(languageCode === 'fr') {
+    console.log("Hello World translated in French is: Bonjour à tous")
+} else {
+    console.log("Hello World in English")
+}
