2885ce0b1fcf3cae668e7684d22be4fbb486a8db
Brenda <ziruhe@gmail.com>
2023-09-28 04:53:08 +0000

    Sep 27, 2023, 9:53 PM

--- Assignment02-module3/js/04-grade-assigner.js
+++ Assignment02-module3/js/04-grade-assigner.js
@@ -1,7 +1,14 @@
-let num = parseInt(prompt("Please enter a number between 1 to 100"))
-if(isNaN(num) || num < 1 || num > 100) {
-    console.log("Please enter a valid number between 1 to 100");
-} else {
-    console.log("valid input")
+let score = parseInt(prompt("Please enter a number between 1 to 100"))
+// if(isNaN(score) || score < 1 || score > 100) {
+//     console.log("Please enter a valid number between 1 to 100");
+// } else {
+//     console.log("valid input")
+
     
+// }
+
+if(score >= 1 && score <= 100) {
+    console.log("valid")
+} else {
+    console.log("invalid")
 }
