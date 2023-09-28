9567b5b75578cb255668c57b3c0a6c1ca1bde240
Brenda <ziruhe@gmail.com>
2023-09-28 04:27:10 +0000

    Sep 27, 2023, 9:27 PM

--- Assignment02-module3/js/03-coinflip.js
+++ Assignment02-module3/js/03-coinflip.js
@@ -1,24 +1,27 @@
 
 let coinFlip = Math.round(Math.random() * 10)
 console.log(coinFlip)
+let choice = prompt('Heads or Tails? Please enter your choice(h/t): ')
 if(coinFlip < 5) { // 0 <= coinFlip < 5  
     result = 'head'
 } else { // 5 <= coinFlip <= 10
     result = 'tail'
 }
 
-let choice = prompt('Heads or Tails? Please enter your choice(h/t): ')
-
 if(result === 'head') { // coin flipped head
     if(choice === 'h') {
         alert("The flip was heads and you chose heads...you win!")
     } else if(choice === 't') {
         alert("The flip was heads but you chose tails...you lose!")
+    } else {
+        alert("Please enter either h or t only")
     }
 } else { // coin flipped tails
     if(choice === 'h') {
         alert("The flip was tails but you chose heads...you lose!")
     } else if(choice === 't') {
         alert("The flip was tails and you chose tails...you win!")
+    } else {
+        alert("Please enter either h or t only")
     }
 }
