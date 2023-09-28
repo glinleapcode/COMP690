67e3c808c3e4dc4be66b73d1f5fbe62cd13896f0
Brenda <ziruhe@gmail.com>
2023-09-28 05:05:00 +0000

    Sep 27, 2023, 10:05 PM

--- Assignment02-module3/js/03-coinflip.js
+++ Assignment02-module3/js/03-coinflip.js
@@ -1,6 +1,7 @@
 
 let coinFlip = Math.round(Math.random() * 10) // generate a random number between [0, 10]
 let choice = prompt('Heads or Tails? Please enter your choice(h/t): ')
+let result
 if(coinFlip < 5) { // 0 <= coinFlip < 5  
     result = 'head'
 } else { // 5 <= coinFlip <= 10
