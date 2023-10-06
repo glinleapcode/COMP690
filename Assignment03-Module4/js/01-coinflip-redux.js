2fcbf168ee38274becbd3cc1e88b03707d9bbafd
Brenda <ziruhe@gmail.com>
2023-10-06 03:03:57 +0000

    Oct 5, 2023, 8:03 PM

--- Assignment03-Module4/js/01-coinflip-redux.js
+++ Assignment03-Module4/js/01-coinflip-redux.js
@@ -1,7 +1,8 @@
 let coinFlip
-numOfFlips = parseInt(prompt("How many times do you want to flip the coin?"))
+numOfFlips = parseInt(prompt("How many times would you like to flip the coin?"))
 for(let i = 0; i < numOfFlips; i++) {
-    coinFlip = Math.round(Math.random())
+    // randomly generate either 0 or 1
+    coinFlip = Math.round(Math.random()) 
     if(coinFlip === 0) {
         console.log("Heads");
     } else if(coinFlip === 1) {
