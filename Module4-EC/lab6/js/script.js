90765a2b4c201e4af6227678209ba15a75dcff8f
Brenda <ziruhe@gmail.com>
2023-10-06 06:05:34 +0000

    Oct 5, 2023, 11:05 PM

--- Module4-EC/lab6/js/script.js
+++ Module4-EC/lab6/js/script.js
@@ -5,9 +5,37 @@ let rate;
 let years;
 
 // COLLECT VALUES FROM THE USER
-investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
-rate = parseFloat(prompt('Enter interest rate as xx.x'));
-years = parseInt(prompt('Enter the number of years you want to invest for'));
+while (true) {
+    investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
+    if (!isNaN(investment)) {
+        break
+    } else {
+        console.log("Invalid investment, please re-enter")
+    }
+}
+
+while (true) {
+    rate = parseFloat(prompt('Enter interest rate as xx.x'));
+    if (!isNaN(rate) && rate >= 0 && rate <= 6) {
+        break
+    } else {
+        console.log("Invalid rate, rate must be between 0 and 6%")
+    }
+}
+
+while (true) {
+    years = parseInt(prompt('Enter the number of years you want to invest for'));
+    if (!isNaN(years) && years >= 1 && years <= 30) {
+        break
+    } else {
+        console.log("Invalid year, years must be between 1 to 30")
+    }
+
+
+}
+
+
+
 
 // CALCULATE FUTURE VALUE
 futureValue = investment;
