49076b615e23443943260c9d3e8578afe5e6e091
Brenda <ziruhe@gmail.com>
2023-10-06 06:11:03 +0000

    Oct 5, 2023, 11:11 PM

--- Module4-EC/lab6/js/script.js
+++ Module4-EC/lab6/js/script.js
@@ -7,10 +7,10 @@ let years;
 // COLLECT VALUES FROM THE USER
 while (true) {
     investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
-    if (!isNaN(investment)) {
+    if (!isNaN(investment) && investment > 0) {
         break
     } else {
-        console.log("Invalid investment, please re-enter")
+        console.log("Invalid investment amount, please re-enter")
     }
 }
 
