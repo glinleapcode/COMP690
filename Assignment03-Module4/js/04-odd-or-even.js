8b3daa8360df9b09573bfe37c3ca6d746157337d
Brenda <ziruhe@gmail.com>
2023-10-06 04:42:01 +0000

    Oct 5, 2023, 9:42 PM

--- Assignment03-Module4/js/04-odd-or-even.js
+++ Assignment03-Module4/js/04-odd-or-even.js
@@ -1,7 +1,7 @@
 for(let i = 0; i <= 15; i++) {
-    if(i % 2 == 0) {
+    if(i % 2 === 0) {
         console.log(`${i} is even`)
-    } else if(i % 2 == 1) {
+    } else if(i % 2 === 1) {
         console.log(`${i} is odd`)
     }
 }
