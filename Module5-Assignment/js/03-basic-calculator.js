796a877139827bee537e1b1e38f75b21be282b97
Brenda <ziruhe@gmail.com>
2023-10-12 06:16:07 +0000

    Oct 11, 2023, 11:16 PM

--- Module5-Assignment/js/03-basic-calculator.js
+++ Module5-Assignment/js/03-basic-calculator.js
@@ -22,9 +22,10 @@ function calculate(x, y, operation) {
 
 // COLLECT FIRST NUMBER FROM USER
 let num1
+// handle invalid number
 while (true) {
     num1 = parseInt(prompt('Enter first number:'))
-    if (!isNaN(num1)) {
+    if (!isNaN(num1)) { // when num1 is valid number break the loop
         break
     }
 }
@@ -32,6 +33,7 @@ while (true) {
 
 // COLLECT SECOND NUMBER FROM USER
 let num2 
+// handle invalid number
 while (true) {
     num2 = parseInt(prompt('Enter second number:'))
     if (!isNaN(num2)) {
@@ -41,6 +43,7 @@ while (true) {
 
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
 let operation
+// handle invalid operation
 while (true) {
     operation = prompt('Enter operation to perform (+,-,*,/)').trim()
     if (operation === '+' || operation === '-' || operation === '*' || operation === '/') {
