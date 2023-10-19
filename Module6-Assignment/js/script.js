5f1769e6414d4aae7314cad3608651a55c2352c2
Brenda <ziruhe@gmail.com>
2023-10-19 23:29:34 +0000

    Oct 19, 2023, 4:29 PM

--- Module6-Assignment/js/script.js
+++ Module6-Assignment/js/script.js
@@ -1,18 +1,45 @@
-const form = document.getElementById('empForm');
-const id = document.getElementById('id');
-const name = document.getElementById('name');
-const ext = document.getElementById('ext');
-const email = document.getElementById('email');
-const department = document.getElementById('department');
-const submit = document.getElementById('submit');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(`ID: ${id.value}`)
-    console.log(`Name: ${name.value}`)
-    console.log(`Ext: ${ext.value}`)
-    console.log(`Email: ${email.value}`)
-    console.log(`Department: ${department.value}`)
+// const form = document.getElementById('empForm');
+// const id = document.getElementById('id');
+// const name = document.getElementById('name');
+// const ext = document.getElementById('ext');
+// const email = document.getElementById('email');
+// const department = document.getElementById('department');
+// const submit = document.getElementById('submit');
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log(`ID: ${id.value}`)
+//     console.log(`Name: ${name.value}`)
+//     console.log(`Ext: ${ext.value}`)
+//     console.log(`Email: ${email.value}`)
+//     console.log(`Department: ${department.value}`)
+// });
+
+window.addEventListener('load', () => {
+    const form = document.getElementById('empForm');
+    const id = document.getElementById('id');
+    const name = document.getElementById('name');
+    const ext = document.getElementById('ext');
+    const email = document.getElementById('email');
+    const department = document.getElementById('department');
+    const submit = document.getElementById('submit');
+    const processForm = (e) => {
+        e.preventDefault();
+        console.log(`ID: ${id.value}`)
+        console.log(`Name: ${name.value}`)
+        console.log(`Ext: ${ext.value}`)
+        console.log(`Email: ${email.value}`)
+        console.log(`Department: ${department.value}`)
+    };
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        processForm(e);
+    })
 });
+
+
+
+
 
 
 
