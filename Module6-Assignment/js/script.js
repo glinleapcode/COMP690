f29577207f92665c2bb140a8d528c3a538c9151d
Brenda <ziruhe@gmail.com>
2023-10-19 23:30:51 +0000

    Oct 19, 2023, 4:30 PM

--- Module6-Assignment/js/script.js
+++ Module6-Assignment/js/script.js
@@ -1,19 +1,3 @@
-// const form = document.getElementById('empForm');
-// const id = document.getElementById('id');
-// const name = document.getElementById('name');
-// const ext = document.getElementById('ext');
-// const email = document.getElementById('email');
-// const department = document.getElementById('department');
-// const submit = document.getElementById('submit');
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     console.log(`ID: ${id.value}`)
-//     console.log(`Name: ${name.value}`)
-//     console.log(`Ext: ${ext.value}`)
-//     console.log(`Email: ${email.value}`)
-//     console.log(`Department: ${department.value}`)
-// });
-
 window.addEventListener('load', () => {
     const form = document.getElementById('empForm');
     const id = document.getElementById('id');
@@ -36,6 +20,24 @@ window.addEventListener('load', () => {
         processForm(e);
     })
 });
+
+
+// const form = document.getElementById('empForm');
+// const id = document.getElementById('id');
+// const name = document.getElementById('name');
+// const ext = document.getElementById('ext');
+// const email = document.getElementById('email');
+// const department = document.getElementById('department');
+// const submit = document.getElementById('submit');
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log(`ID: ${id.value}`)
+//     console.log(`Name: ${name.value}`)
+//     console.log(`Ext: ${ext.value}`)
+//     console.log(`Email: ${email.value}`)
+//     console.log(`Department: ${department.value}`)
+// });
+
 
 
 
