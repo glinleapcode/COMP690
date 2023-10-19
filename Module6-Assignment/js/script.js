b4ddec9fa968c22f380601f5160fd97071fc40d4
Brenda <ziruhe@gmail.com>
2023-10-19 22:27:12 +0000

    Oct 19, 2023, 3:27 PM

---
+++ Module6-Assignment/js/script.js
@@ -0,0 +1,19 @@
+
+const id = document.getElementById('id');
+const name = document.getElementById('name');
+const ext = document.getElementById('ext');
+const email = document.getElementById('email');
+//department is a select element
+const department = document.getElementById('department');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(`ID: ${id.value}`)
+    console.log(`Name: ${name.value}`)
+    console.log(`Ext: ${ext.value}`)
+    console.log(`Email: ${email.value}`)
+    console.log(`Department: ${department.value}`)
+
+});
+
