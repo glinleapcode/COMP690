b81bf93ea12546ebde7d3f2f428e7f2d3490698b
Brenda <ziruhe@gmail.com>
2023-12-05 06:13:10 +0000

    Dec 4, 2023, 10:13 PM

--- M9-assignment/js/script.js
+++ M9-assignment/js/script.js
@@ -21,8 +21,7 @@ empTable.addEventListener("click", (e) => {
       // REMOVE EMPLOYEE FROM ARRAY
       empTable.deleteRow(rowIndex);
       // UPDATE EMPLOYEE COUNT
-      empCount.value = `(${empTable.rows.length - 1})`;
-      console.log(empTable);
+      empCount.value = `(${empTable.rows.length - 1})`; // -1 for the header row
     }
   }
 });
