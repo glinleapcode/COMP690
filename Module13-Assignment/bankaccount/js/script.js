508d9e8da541a8410f5f6cab590478d0231f3929
Brenda <ziruhe@gmail.com>
2024-01-24 17:15:07 +0000

    Jan 24, 2024, 9:15 AM

---
+++ Module13-Assignment/bankaccount/js/script.js
@@ -0,0 +1,61 @@
+const bankAccount = function (ownerName) {
+  let balance = 0;
+  let name = ownerName;
+  return {
+    withdraw: function (withdrawalAmount) {
+      if (withdrawalAmount > balance) {
+        console.log("Insufficient funds");
+        return null;
+      }
+      balance -= withdrawalAmount;
+      return balance;
+    },
+    deposit: function (depositAmount) {
+      balance += depositAmount;
+      return balance;
+    },
+    getOwnerName: function () {
+      return name;
+    },
+  };
+};
+
+const nameBtn = document.getElementById("name");
+const depositBtn = document.getElementById("deposit");
+const withdrawBtn = document.getElementById("withdraw");
+const display = document.getElementById("display");
+
+let account;
+nameBtn.addEventListener("click", () => {
+  const name = prompt("Enter your name");
+  console.log(name);
+  display.innerHTML = `Welcome to your bank account: Name: ${name}`;
+  account = bankAccount(name);
+});
+
+depositBtn.addEventListener("click", () => {
+  const amount = parseFloat(prompt("Enter the amount to deposit"));
+  if (isNaN(amount) || amount <= 0) {
+    alert("Please enter a valid amount");
+    return;
+  }
+  const newBalance = account.deposit(amount);
+  display.innerHTML = `Name: ${account.getOwnerName()} <br> New Balance: ${newBalance} <br>
+   Recent Transaction: depositing ${amount} `;
+  console.log(amount, typeof amount);
+});
+
+withdrawBtn.addEventListener("click", () => {
+  const amount = parseFloat(prompt("Enter the amount to withdraw"));
+  if (isNaN(amount) || amount <= 0) {
+    alert("Please enter a valid amount");
+    return;
+  }
+  const newBalance = account.withdraw(amount);
+  if (newBalance === null) {
+    alert("Insufficient funds for withdrawal.");
+    return;
+  }
+  display.innerHTML = `Name: ${account.getOwnerName()} <br> New Balance: ${newBalance} <br> Recent Transaction: withdrawing ${amount} `;
+  console.log(amount, typeof amount);
+});
---
+++ Module13-Assignment/slideshow/js/slideshow.js
@@ -0,0 +1,134 @@
+// REWRITTEN TO TAKE ADVANTAGE OF CLOSURES
+const $ = (id) => document.getElementById(id);
+
+const createSlideshow = function () {
+  // PRIVATE VARIABLES AND FUNCTIONS
+  let timer;
+  let play = true;
+  // nodes store the current image and caption
+  let nodes = { image: null, caption: null };
+  let img = { cache: [], counter: 0 };
+  let speed = 2000;
+
+  const stopSlideShow = function () {
+    clearInterval(timer);
+  };
+
+  const displayNextImage = function () {
+    let image = img.cache[img.counter];
+    nodes.image.src = image.src;
+    nodes.caption.innerHTML = image.title;
+    img.counter++;
+    if (img.counter === img.cache.length) {
+      img.counter = 0;
+    }
+  };
+
+  const setPlayText = function (btn) {
+    if (play) {
+      btn.value = "Resume";
+    } else {
+      btn.value = "Pause";
+    }
+  };
+
+  // PUBLIC METHODS THAT HAVE ACCESS TO PRIVATE VARIABLES AND FUNCTIONS
+  return {
+    loadImages: function (slides) {
+      for (let i = 0; i < slides.length; i++) {
+        let image = new Image();
+        image.src = slides[i].href;
+        image.title = slides[i].title;
+        img.cache.push(image);
+      }
+      // this refer to the object that the createSlideshow() method returns
+      // return this allows method chaining of loadImages() and startSlideShow()
+      return this;
+    },
+    startSlideShow: function () {
+      if (arguments.length === 2) {
+        nodes.image = arguments[0];
+        nodes.caption = arguments[1];
+      }
+      //   timer = setInterval(displayNextImage, 2000);
+      console.log(this.getSpeed());
+      timer = setInterval(displayNextImage, this.getSpeed());
+      return this;
+    },
+    createToggleHandler: function () {
+      // this refers to the slideshow object
+      let me = this;
+      // CLOSURE TO BE USED AS THE CLICK EVENT HANDLER
+      return function () {
+        // 'THIS' IS THE CLICKED BUTTON
+        // 'ME' IS THE OBJECT LITERAL
+        if (play) {
+          stopSlideShow();
+        } else {
+          me.startSlideShow();
+        }
+        // this refers to the clicked button
+        // set playButton text to 'Resume' or 'Pause'
+        setPlayText(this);
+        // TOGGLE PLAY 'FLAG'
+        play = !play;
+      };
+    },
+
+    setSpeed: function (newSpeed) {
+      speed = newSpeed;
+      return this;
+    },
+    getSpeed: function () {
+      return speed;
+    },
+
+    createSetSpeedHandler: function () {
+      let me = this;
+      console.log(me);
+      return function () {
+        let newSpeed = parseInt(
+          prompt("Please enter a new speed in milliseconds: ")
+        );
+        if (!isNaN(newSpeed) && newSpeed > 0) {
+          stopSlideShow();
+          me.setSpeed(newSpeed);
+          me.startSlideShow(nodes.image, nodes.caption);
+
+          //   console.log(newSpeed);
+        } else {
+          alert("Please enter a valid speed in milliseconds.");
+        }
+      };
+    },
+  };
+};
+
+// CREATE THE SLIDESHOW OBJECT
+const slideshow = createSlideshow();
+
+window.addEventListener("load", () => {
+  const slides = [
+    { href: "images/backpack.jpg", title: "He backpacks in the Sierras often" },
+    { href: "images/boat.jpg", title: "He loves his boat" },
+    { href: "images/camaro.jpg", title: "He loves his Camaro more" },
+    {
+      href: "images/punk.jpg",
+      title:
+        "He used to be in a punk band and toured with No Doubt and Sublime",
+    },
+    {
+      href: "images/race.jpg",
+      title: "He's active and loves obstacle coarse racing",
+    },
+    {
+      href: "images/wakesurf.jpg",
+      title: "He loves to wakesurf behind his boat",
+    },
+  ];
+  // START THE SLIDESHOW
+  slideshow.loadImages(slides).startSlideShow($("image"), $("caption"));
+  // PAUSE THE SLIDESHOW
+  $("play_pause").onclick = slideshow.createToggleHandler();
+  $("set_speed").onclick = slideshow.createSetSpeedHandler();
+});
